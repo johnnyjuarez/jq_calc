1c7c8efa8dd0566b106f2bd112f8225e1725567b
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-05-21 16:25:32 -0500

    completed test -2

--- index.js
+++ index.js
@@ -1,113 +1,15 @@
-$(() => {
-    const clear = $("#clear")
-    const display = $("#display")
-    const equals = $("#equals")
-    const add = $("#add")
-    const subtract = $("#subtract")
-    const multiply = $("#multiply")
-    const divide = $("#divide")
-    let holdArray = []
-    let numArr = []
-    let formulaArray = []
-    let firstNum;
-    let secondNum;
-    let result;
-    display.val(numArr)
-
-    let logicOperator;
-
-
-    // display function
-    const displayFunc = () => {
-        display.val(holdArray.join(""))
+function insrt(num) {
+    if ($(".display").val() === "0") {
+        $(".display").val("")
     }
-    // push selected numbers into numArr
-    const numPush = (num) => {
-        if (numArr[0] === 0) {
-            numArr = []
-        }
-        holdArray.push(num)
-        displayFunc()
+    if ($(".display").val().includes('.')) {
+        $(".display").val()
     }
-    $("#one").click(() => {
-        numPush(1)
-    })
-    $("#two").click(() => {
-        numPush(2)
-    })
-    $("#three").click(() => {
-        numPush(3)
-    })
-    $("#four").click(() => {
-        numPush(4)
-    })
-    $("#five").click(() => {
-        numPush(5)
-    })
-    $("#six").click(() => {
-        numPush(6)
-    })
-    $("#seven").click(() => {
-        numPush(7)
-    })
-    $("#eight").click(() => {
-        numPush(8)
-    })
-    $("#nine").click(() => {
-        numPush(9)
-    })
-    $("#zero").click(() => {
-        numPush(0)
-    })
-
-    // clearing the calculator
-    clear.click(() => {
-        console.log("clicked")
-        holdArray = [0]
-        numArr = []
-        displayFunc()
-    })
-    // logical operator functions
-    add.click(() => {
-        firstNum = 0;
-        console.log("add clicked")
-        numArr.push(parseInt(holdArray.join('')))
-        numArr.push('+')
-        holdArray = []
-        console.log(numArr)
-        displayFunc()
-    })
-    subtract.click(() => {
-        firstNum = 0;
-        console.log("subtract clicked")
-        logicOperator = '-';
-        firstNum = parseInt(numArr.join(''))
-        numArr = []
-        displayFunc()
-    })
-    multiply.click(() => {
-        firstNum = 0;
-        console.log("multiply clicked")
-        logicOperator = '*';
-        firstNum = parseInt(numArr.join(''))
-        numArr = []
-        displayFunc()
-    })
-    divide.click(() => {
-        firstNum = 0;
-        console.log("divide clicked")
-        logicOperator = '/';
-        firstNum = parseInt(numArr.join(''))
-        numArr = []
-        displayFunc()
-    })
-    equals.click(() => {
-        numArr.push(parseInt(holdArray.join('')))
-        numArr.forEach((num, i, arr) => {
-            let result = num[i]
-            console.log("This is result: " + result)
-            console.log(arr[i])
-        })
-    })
-
-})
+    $(".display").val($(".display").val() + num)
+}
+function eql() {
+    $(".display").val(eval($(".display").val()))
+}
+function c() {
+    $('.display').val('0')
+}
